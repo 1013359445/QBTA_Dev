53c035d78aade597f299fa01f92de755c4d5046f
白伟 <1013359445@qq.com>
2023-03-17 11:01:05 +0800

    公告优化

--- QB_TA_iOS/IOSFramework/Modules/ControlPanel/TAAnnouncementView.m
+++ QB_TA_iOS/IOSFramework/Modules/ControlPanel/TAAnnouncementView.m
@@ -28,6 +28,7 @@ shareInstance_implementation(TAAnnouncementView);
 {
     if (self = [super init]) {
         self.clipsToBounds = YES;
+        self.userInteractionEnabled = NO;
         [self creatSubViews];
     }
     return self;
@@ -64,6 +65,7 @@ shareInstance_implementation(TAAnnouncementView);
         [self showText];
     }
 }
+//#import "UILabel+JKSuggestSize.h"
 
 - (void)showText
 {
@@ -80,20 +82,25 @@ shareInstance_implementation(TAAnnouncementView);
     contentLabel.textColor = kTAColor.c_49;
     contentLabel.font = [UIFont systemFontOfSize:10];
     contentLabel.marqueeLabelType = KJMarqueeLabelTypeLeft;
-    contentLabel.speed = 0.5;
     contentLabel.secondLabelInterval = 40;
     contentLabel.stopTime = 5;
     contentLabel.text = text;
+    contentLabel.speed = 0.6;
     [self addSubview:contentLabel];
 
+    CGSize size = [contentLabel jk_suggestSizeForString:text width:9999];
+    CGFloat time = 7.4;
+    if (size.width > self.bounds.size.width) {
+        time = size.width / self.bounds.size.width * time;
+    }
     kWeakSelf(self);
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(time * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [contentLabel removeFromSuperview];
         weakself.bgImageView.alpha = 0;
         [weakself.contentQueue removeObjectAtIndex:0];
     });
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(13 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)((time + 3) * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [weakself showText];
     });
 }
